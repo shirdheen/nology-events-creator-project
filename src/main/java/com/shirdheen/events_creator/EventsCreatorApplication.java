0792bae9bf1696a59867b3900fe922a5dc1d3531
shirdheen <shirdheen2252@gmail.com>
2025-04-08 22:22:41 +1000

    Backend setup

---
+++ src/main/java/com/shirdheen/events_creator/EventsCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.shirdheen.events_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shirdheen/events_creator/EventsCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirdheen.events_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
