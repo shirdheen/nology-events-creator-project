1774d8668545f0928915ba24474e8ce0841d572c
shirdheen <shirdheen2252@gmail.com>
2025-05-02 10:37:43 +1000

    Modifies CORS config

--- src/main/java/com/shirdheen/events_creator/config/CorsConfig.java
+++ src/main/java/com/shirdheen/events_creator/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173", "https://shirdheen.github.io/nology-events-creator-project/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
     }
 }
